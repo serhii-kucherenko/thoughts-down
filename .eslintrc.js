0cfee2a1dff2d7e58791e21c5e2bdc5e655ae7ad
Kucherenko Sergey <kucherenko.web@gmail.com>
2020-01-27 21:08:56 +0200

    Add TS support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,14 @@
 module.exports = {
     root: true,
     env: {
-        node: true,
-        browser: true
+        node: true
     },
     extends: [
-        'plugin:vue/recommended',
-        'eslint:recommended',
+        '@nuxtjs',
+        '@nuxtjs/eslint-config-typescript',
+        'prettier',
         'prettier/vue',
-        'plugin:prettier/recommended'
-    ],
-    rules: {
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    },
-    globals: {
-        $nuxt: true
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    }
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended'
+    ]
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,29 @@
 <template>
   <div>
     <el-row>
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button plain>Plain</el-button>
-      <el-button type="primary" plain>Primary</el-button>
-      <el-button type="success" plain>Success</el-button>
-      <el-button type="info" plain>Info</el-button>
-      <el-button type="warning" plain>Warning</el-button>
-      <el-button type="danger" plain>Danger</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button round>Round</el-button>
-      <el-button type="primary" round>Primary</el-button>
-      <el-button type="success" round>Success</el-button>
-      <el-button type="info" round>Info</el-button>
-      <el-button type="warning" round>Warning</el-button>
-      <el-button type="danger" round>Danger</el-button>
-    </el-row>
-
-    <el-row>
-      <el-button icon="el-icon-search" circle></el-button>
-      <el-button type="primary" icon="el-icon-edit" circle></el-button>
-      <el-button type="success" icon="el-icon-check" circle></el-button>
-      <el-button type="info" icon="el-icon-message" circle></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+      <h2>{{ message }}</h2>
+      <h4>{{ fullName }}</h4>
     </el-row>
   </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+interface User {
+  firstName: string
+  lastName: string
+}
+
+@Component
+export default class YourComponent extends Vue {
+  user: User = { firstName: 'Sergey', lastName: 'Kucherenko' }
+  message: string = 'TS works'
+
+  get fullName(): string {
+    return `${this.user.firstName} ${this.user.lastName}`
+  }
+}
 </script>
 
 <style lang="scss">
